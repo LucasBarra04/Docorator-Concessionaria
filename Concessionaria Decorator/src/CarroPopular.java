416ac810250f644e7bfd5dfa07be0411d0abe3cd
Lucas Barra Machado <lucasbm.jf@gmail.com>
2026-04-08 14:31:10 -0300

    Classes tipos de Carro

---
+++ Concessionaria Decorator/src/CarroPopular.java
@@ -0,0 +1,11 @@
+public class CarroPopular implements Carro {
+    @Override
+    public String getDescricao() {
+        return "Carro Popular";
+    }
+
+    @Override
+    public double getPreco() {
+        return 70000.00;
+    }
+}
---
+++ Concessionaria Decorator/src/SUV.java
@@ -0,0 +1,11 @@
+public class SUV implements Carro{
+    @Override
+    public String getDescricao() {
+        return "SUV";
+    }
+
+    @Override
+    public double getPreco() {
+        return 100000.00;
+    }
+}
